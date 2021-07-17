c5c572209cc1dcbd2945b0e7b7ce81d850e54ae7
David Quintero <davidq@gmail.com>
2021-07-17 15:28:00 -0400

    Grouping Digits: apprenticeship challenge

--- gDigits.js
+++ gDigits.js
@@ -1,10 +1,3 @@
-/*
- * Complete the 'minMoves' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
 function minMoves(arr) {
   // Write your code here
 
@@ -22,6 +15,7 @@ function minMoves(arr) {
     }
   };
 
+  // If array is sorted return zero
   if (isSorted(arr)) return 0;
 
   // Swap values if array is unsorted
@@ -29,7 +23,6 @@ function minMoves(arr) {
   let sortedArray = [...arr];
 
   while (!isSorted(sortedArray)) {
-    let firstZeroIndex = sortedArray.indexOf(0);
     for (let i = 0; i < sortedArray.length; i++) {
       if (sortedArray[i] === 0 && sortedArray[i + 1] === 1) {
         let swapOne = sortedArray[i + 1];
@@ -37,18 +30,9 @@ function minMoves(arr) {
         sortedArray.splice(i, 1, swapOne);
         sortedArray.splice(i + 1, 1, swapZero);
         swapCount++;
-        console.log('FIRST');
-      } else if (sortedArray[i] === 1 && firstZeroIndex !== -1) {
-        let swapOne = sortedArray[i];
-        let swapZero = sortedArray[firstZeroIndex];
-        sortedArray.splice(firstZeroIndex, 1, swapOne);
-        sortedArray.splice(i, 1, swapZero);
-        swapCount++;
-        console.log('ELSE');
       }
     }
   }
-
   return swapCount;
 }
 
@@ -70,16 +54,6 @@ let arrCaseThr = [1, 0, 1, 0, 0, 0, 0, 1]; // 6
 //  [1, 1, 1, 0, 0, 0, 0, 0]  // 6
 
 let edgeCase = [1, 0, 0, 1, 0, 1, 1, 0, 0, 1];
-// [1, 0, 0, 1, 0, 1, 1, 0, 0, 1]
-// [1, 0, 1, 0, 0, 1, 1, 0, 0, 1] // 1
-// [1, 1, 0, 0, 0, 1, 1, 0, 0, 1] // 2
-// [1, 1, 0, 0, 1, 0, 1, 0, 0, 1] // 3
-// [1, 1, 0, 1, 0, 0, 1, 0, 0, 1] // 4
-// [1, 1, 1, 0, 0, 0, 1, 0, 0, 1] // 5
-// [1, 1, 1, 0, 0, 1, 0, 0, 0, 1] // 6
-// [1, 1, 1, 0, 1, 0, 0, 0, 0, 1] // 7
-// [1, 1, 1, 1, 0, 0, 0, 0, 0, 1] // 8
-// [1, 1, 1, 1, 1, 0, 0, 0, 0, 0] // 9
 
 // console.log(minMoves(arrCaseOne));
 // console.log(minMoves(arrCaseTwo));
