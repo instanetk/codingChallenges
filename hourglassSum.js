f818856e3afb0d9c517cc38610d355adef07a393
David Quintero <davidq@gmail.com>
2021-11-09 13:28:04 -0500

    hourglassSum, baby

--- hourglassSum.js
+++ hourglassSum.js
@@ -1,5 +1,29 @@
 function hourglassSum(arr) {
   // Write your code here
+
+  let arraySums = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < arr.length - 2; k++) {
+      let high, middle, low;
+      if (i + 2 < 6) {
+        high = arr[i][k] + arr[i][k + 1] + arr[i][k + 2];
+        middle = arr[i + 1][k + 1];
+        low = arr[i + 2][k] + arr[i + 2][k + 1] + arr[i + 2][k + 2];
+      }
+
+      let sum = high + middle + low;
+
+      if (!isNaN(sum)) {
+        arraySums.push(sum);
+      } else {
+        i++;
+      }
+    }
+  }
+  arraySums.sort((a, b) => b - a);
+
+  return arraySums[0];
 }
 
 let arr = [
@@ -10,6 +34,8 @@ let arr = [
   [0, 0, 0, -2, 0, 0],
   [0, 0, 1, 2, 4, 0],
 ];
+
+console.log(hourglassSum(arr));
 
 // Sixteen hourglasses
 // -63, -34, -9, 12,
